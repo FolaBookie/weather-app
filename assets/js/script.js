85dde33cf4689ac42f5e494d85f777abc81c7115
FolaBookie <folabookie@gmail.com>
2024-01-16 23:23:56 +0000

    added the geocoding fetch function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,25 @@
+const WEATHER_API_KEY = "91aa83961bdb15731638dab6ce61e046";
+
+function generateGeocodingEndpoint(city) {
+  return `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${WEATHER_API_KEY}`;
+}
+
+function fetchCityGeocodes(endpoint) {
+  fetch(endpoint)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+}
+$("#search-form").on("submit", function (e) {
+  e.preventDefault();
+  const inputValue = $("#search-input").val();
+  const geocodingEndpoint = generateGeocodingEndpoint(inputValue);
+  fetchCityGeocodes(geocodingEndpoint);
+});
+
 // Create a weather dashboard with form inputs.
 // When a user searches for a city they are presented with current and future conditions for that city and that city is added to the search history
 // When a user views the current weather conditions for that city they are presented with:
